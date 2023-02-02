463ed134c1b9c06ed744e41a57af2739c288ce61
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2023-02-02 21:36:34 +0800

    测试

--- function/rj/8du.py
+++ function/rj/8du.py
@@ -17,10 +17,11 @@ response = requests.request("GET", url, headers=headers)
 page_content = response.text
 # print(page_content)
 
-obj=re.compile(r'<a href=.*.cp-post-cat>(?P<fenqu>.*?)</a>\n.*\n.*\n.*\n.*\n.*\n<a href="(?P<dizi>.*?)".*an>(?P<name>.*?)</a>\n.*\n.*\n(?P<mos>.*?)\n')
+# obj=re.compile(r'<a href=.*.cp-post-cat>(?P<fenqu>.*?)</a>\n.*\n.*\n.*\n.*\n.*\n<a href="(?P<dizi>.*?)".*an>(?P<name>.*?)</a>\n.*\n.*\n(?P<mos>.*?)\n')
 # obj = re.compile(r'<a href=.*?cp-post-cat>(?P<fenqu>.*?)</a>\n.*\n.*\n.*\n.*\n.*\n'
 #                  r'<a href="(?P<dizi>.*?)" target="_self"></span>(?P<name>.*?)</a>'
 #                  r'<p cp-post-excerpt>\n(?P<mos>.*?)\n')
+obj=re.compile(r'<a href=.*.cp-post-cat>(?P<fenqu>.*?)</a>.*<a href="(?P<dizi>.*?)".*an>(?P<name>.*?)</a>.*<p cp-post-excerpt>(?P<mos>.*?)\n',re.S)
 
 result = obj.finditer(page_content)
 for it in result:
@@ -28,7 +29,7 @@ for it in result:
     fenqu=it.group("fenqu")
     name=it.group("name")
     描述=it.group("mos")
-    print("地址：%s\t分区：%s\t软件名：%s\t描述：%s" % (dizi, fenqu, name, 描述))
+    print("分区：%s\t软件名：%s\t描述：%s\t地址：%s" % (fenqu, name, 描述, dizi))
     print("完成？")
 
 # print(response.text)
