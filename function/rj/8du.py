5f0e6aa3b9c271c99856740f23ca273302e6890c
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2023-02-02 20:11:51 +0800

    Update 8du.py

--- function/rj/8du.py
+++ function/rj/8du.py
@@ -19,8 +19,8 @@ page_content = response.text
 obj = re.compile(r'<a href=".*?"target=".*?"cppost-cat=".*?"class="link-instanted">(?P<fenqu>.*?)</a>' )
 result = obj.finditer(page_content)
 for it in result:
-    fqu=it.group('id')
-    print(fqu)
+    fqu=it.group('fenqu')
+    print(it.group("fenqu"))
     # print(it.group("id"))
     # print(it.group("wahaha"))
 
