734fb992410ba7edc3dba6773dcccdf9115b5fc4
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2023-02-02 19:37:22 +0800

    Update 8du.py

--- function/rj/8du.py
+++ function/rj/8du.py
@@ -8,6 +8,6 @@ url='https://ruanjianku.cloud/'
 headers = {
    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.70'
 }
-response = requests.request("GET", url, headers=headers, data=payload)
+response = requests.request("GET", url, headers=headers)
 
 print(response.text)
