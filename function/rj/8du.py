82393b2969862b15a2b3852a4f650ed95809d74d
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2023-02-02 22:24:10 +0800

    Update 8du.py

--- function/rj/8du.py
+++ function/rj/8du.py
@@ -9,7 +9,6 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.70'
 }
 
-
 response = requests.request("GET", url, headers=headers)
 page_content = response.text
 
