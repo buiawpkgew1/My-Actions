127fce45f9be6f23201dd39568b6f8df0cf170b6
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2023-02-02 20:52:17 +0800

    Update 8du.py

--- function/rj/8du.py
+++ function/rj/8du.py
@@ -17,21 +17,23 @@ response = requests.request("GET", url, headers=headers)
 page_content = response.text
 
 # obj = re.compile(r'<a href=".*" target=".*" cp-post-cat>(?P<fenqu>.*?)</a>',re.S)
-
-obj = re.compile(r'<a href="(?P<dizi>.*?)" target="_self"></span>(?P<name>.*?)</a>'
-                 r'<a href=".*" target=".*" cp-post-cat>(?P<fenqu>.*?)</a>'
-                 r'<p cp-post-excerpt>\n(?P<mos>.*?)\n</p>'
-                 r'', re.S)
+obj=re.compile(r'<a href="(?P<dizi>.*?)" target="_self"></span>(?P<name>.*?)</a>\n</h2>\n<p cp-post-excerpt>\n(?P<mos>.*?)\n</p>')
+# obj = re.compile(r'<a href="(?P<dizi>.*?)" target="_self"></span>(?P<name>.*?)</a>'
+#                 #  r'<a href=".*" target=".*" cp-post-cat>(?P<fenqu>.*?)</a>'
+#                  r'<p cp-post-excerpt>\n(?P<mos>.*?)\n</p>')
 
 result = obj.finditer(page_content)
 for it in result:
     diz=it.group("dizi")
-    分区=it.group("fenqu")
+    # 分区=it.group("fenqu")
     name=it.group("name")
     描述=it.group("mos")
     print("地址：%s\t分区：%s\t软件名：%s\t描述：%s" % (diz, 分区, name, 描述))
     # fqu=it.group('fenqu')
     # print(it.group("fenqu").strip())
+
+    # print(it.group("dizi"))
+
     print("完成!")
 
 # print(response.text)
