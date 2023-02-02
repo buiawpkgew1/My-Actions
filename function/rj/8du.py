6c7762cf73b1f49d12f90c0ab8d87ffa80e2cbcd
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2023-02-02 20:20:19 +0800

    Update 8du.py

--- function/rj/8du.py
+++ function/rj/8du.py
@@ -16,7 +16,7 @@ headers = {
 response = requests.request("GET", url, headers=headers)
 page_content = response.text
 
-obj = re.compile(r'<a href=".*?"target=".*?"cppost-cat=".*?"class="link-instanted">(?P<fenqu>.*?)</a>' )
+obj = re.compile(r'<a href=".*" target=".*" cp-post-cat>(?P<fenqu>.*?)</a>' )
 result = obj.finditer(page_content)
 for it in result:
     fqu=it.group('fenqu')
