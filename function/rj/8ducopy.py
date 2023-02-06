4e7880abf0fd049230d27adef6c20b31d52281bc
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2023-02-06 18:04:16 +0800

    Update 8ducopy.py

--- function/rj/8ducopy.py
+++ function/rj/8ducopy.py
@@ -8,20 +8,20 @@ import random
 import requests
 sys.path.append("My-Actions/function/rj/")
 
-url='https://ruanjianku.cloud/'
-headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.70'
-}
+# url='https://ruanjianku.cloud/'
+# headers = {
+#     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.70'
+# }
 
-response = requests.request("GET", url, headers=headers)
-page_content = response.text
+# response = requests.request("GET", url, headers=headers)
+# page_content = response.text
 
-obj = re.compile(r'<a href=".*?cp-post-cat>(?P<fenqu>.*?)</a>.*?'
-                 r'<a href="(?P<dizi>.*?)".*?'
-                 r'lf"></span>(?P<name>.*?)</a>.*?'
-                 r'<p cp-post-excerpt>\n(?P<mos>.*?)</p>.*?</div>', re.S)
+# obj = re.compile(r'<a href=".*?cp-post-cat>(?P<fenqu>.*?)</a>.*?'
+#                  r'<a href="(?P<dizi>.*?)".*?'
+#                  r'lf"></span>(?P<name>.*?)</a>.*?'
+#                  r'<p cp-post-excerpt>\n(?P<mos>.*?)</p>.*?</div>', re.S)
 
-result = obj.findall(page_content)
+# result = obj.findall(page_content)
 
 # print(result)
 
