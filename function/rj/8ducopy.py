e9a8641ff901d2253b7495987832552cb095e63b
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2023-02-06 16:45:47 +0800

    Update 8ducopy.py

--- function/rj/8ducopy.py
+++ function/rj/8ducopy.py
@@ -8,7 +8,7 @@ import requests
 sys.path.append("My-Actions/function/rj/")
 
 
-baseurl = 'https://ruanjianku.cloud/'
+baseurl = 'https://movie.douban.com/top250?start='
 def geturl(url):
     headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.70'
